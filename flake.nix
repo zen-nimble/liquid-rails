c1d9c587f3e04f823f551d53a228431b42b41723
Arthur Andersen <leoc.git@gmail.com>
2024-01-05 12:50:11 +0100

    Add direnv and flake.nix

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Developer environment shell for ERICA SaaS";
+
+  inputs = {  };
+
+  outputs = { self, nixpkgs }:
+    let
+      # Helper to provide system-specific attributes
+      forAllSupportedSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+
+      supportedSystems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+    in
+
+    {
+      devShells = forAllSupportedSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            ruby_3_2
+            imagemagick
+            libyaml
+            cmake
+          ];
+        };
+      });
+    };
+}
